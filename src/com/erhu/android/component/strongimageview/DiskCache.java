4d79ff07af11fbe68ed0979ed2a052c5913f8d25
erhu <erhu.cn@qq.com>
2014-03-19 20:31:50 +0800

    持久化最近访问的图片文件；

--- src/com/erhu/android/component/strongimageview/DiskCache.java
+++ src/com/erhu/android/component/strongimageview/DiskCache.java
@@ -2,12 +2,14 @@ package com.erhu.android.component.strongimageview;
 
 import android.util.Log;
 import com.erhu.android.component.BitmapLoader;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -21,82 +23,154 @@ enum DiskCache {
 
     INSTANCE(BitmapLoader.getInstance().getDir(), BitmapLoader.getInstance().imgNameStrategy);
 
-    String dir;
-    ImageCacheNameStrategy imgNameStrategy;
+    private String dir;
+    private ImageCacheNameStrategy imgNameStrategy;
+
+    private static final int SIZE = 10;
+
+    // concurrent Set :)
+    // 删除冗余图片时，快速定位图片是否在缓存中。
+    private Set<String> diskCacheUrlSet = Collections.newSetFromMap(new ConcurrentHashMap<String, Boolean>());
+    // 删除冗余图片时，按物理文件名称，快速定位图片。
+    private Set<String> diskCacheFileNameSet = Collections.newSetFromMap(new ConcurrentHashMap<String, Boolean>());
+
+    // 用于存储，当缓存图片数超标时，从链表尾部开始删除。
+    private List<CacheEntry> diskCacheList = Collections.synchronizedList(new ArrayList<CacheEntry>(SIZE));
 
     private DiskCache(String _dir, ImageCacheNameStrategy _strategy) {
         this.dir = _dir;
         this.imgNameStrategy = _strategy;
 
-        // 每5分钟执行一次冗余图片删除操作
-        ScheduledExecutorService ses = Executors.newScheduledThreadPool(1);
-        ses.scheduleAtFixedRate(new Runnable() {
-            @Override
-            public void run() {
-                // 删除引用
-                Iterator iterator = diskCacheList.iterator();
-                while (diskCacheList.size() > SIZE && iterator.hasNext()) {
-                    int last_index = diskCacheList.size() - 1;
-                    CacheEntry last_entry = diskCacheList.get(last_index);
+        if (dumpFile().exists()) {
+            // 从DUMP文件读取数据（1分钟后，只一次）
+            Executors.newScheduledThreadPool(1).schedule(
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            // 保存引用数据到文件中
+                            ObjectMapper mapper = new ObjectMapper();
+                            try {
+                                List list = mapper.readValue(dumpFile(), ArrayList.class);
+                                for (Object o : list) {
+                                    HashMap<String, String> map = (HashMap<String, String>) o;
+                                    CacheEntry entry = new CacheEntry();
+                                    entry.url = map.get("url");
+                                    addEntry(entry);
+                                }
 
-                    diskCacheList.remove(last_index);
-                    diskCacheUrlSet.remove(last_entry.url);
-                    diskCacheFileNameSet.remove(imgNameStrategy.getName(last_entry.url));
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            for (CacheEntry ce : diskCacheList) {
+                                Log.d(StrongImageViewConstants.TAG, ce.getUrl() + " _ 2");
+                            }
+                        }
+                    }, 1, TimeUnit.SECONDS
+            );
+        }
 
-                    if (StrongImageViewConstants.IS_DEBUG) {
-                        Log.d(StrongImageViewConstants.TAG, "del image ref");
-                    }
-                }
+        // 2分钟后开始检查，然后每10分钟执行一次冗余图片删除操作。
+        Executors.newScheduledThreadPool(1).
+                scheduleAtFixedRate(new Runnable() {
+                    @Override
+                    public void run() {
+                        // 删除引用
+                        Iterator iterator = diskCacheList.iterator();
+                        while (diskCacheList.size() > SIZE && iterator.hasNext()) {
+                            int last_index = diskCacheList.size() - 1;
+                            removeEntry(diskCacheList.get(last_index));
 
-                // 获取缓存图片文件夹中所有的图片
-                File[] file_array = new File(BitmapLoader.getInstance().getDir()).listFiles();
-                // 删除物理文件
-                for (File f : file_array) {
-                    if (!diskCacheFileNameSet.contains(f.getName())) {
-                        // 删除图片文件
-                        boolean b = f.delete();
-                        if (StrongImageViewConstants.IS_DEBUG) {
-                            Log.d(StrongImageViewConstants.TAG, "del image file:" + b);
+                            diskCacheList.remove(last_index);
+
+                            if (StrongImageViewConstants.IS_DEBUG) {
+                                Log.d(StrongImageViewConstants.TAG, "del image ref");
+                            }
+                        }
+
+                        // 保存引用数据到文件中
+                        ObjectMapper mapper = new ObjectMapper();
+                        try {
+                            for (CacheEntry ce : diskCacheList) {
+                                Log.d(StrongImageViewConstants.TAG, ce.getUrl() + " _ 3");
+                            }
+                            mapper.writeValue(dumpFile(), diskCacheList);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        // 获取缓存图片文件夹中所有的图片(非下划线开始)
+                        final File[] file_array = new File(dir).listFiles(new FilenameFilter() {
+                            @Override
+                            public boolean accept(File dir, String filename) {
+                                return !filename.startsWith("_");
+                            }
+                        });
+
+                        // 删除物理文件
+                        for (File f : file_array) {
+                            if (!diskCacheFileNameSet.contains(f.getName())) {
+                                // 删除图片文件
+                                boolean b = f.delete();
+                                if (StrongImageViewConstants.IS_DEBUG) {
+                                    Log.d(StrongImageViewConstants.TAG, "del image file:" + b);
+                                }
+                            }
                         }
                     }
-                }
-            }
-        }, 10, 40, TimeUnit.SECONDS);
+                }, 20, 40, TimeUnit.SECONDS);
     }
-
-    private static final int SIZE = 10;
-
-    // concurrent Set :)
-    // 删除冗余图片时，使用MAP快速确定，某图片是否需要保留
-    private Set<String> diskCacheUrlSet = Collections.newSetFromMap(new ConcurrentHashMap<String, Boolean>());
-    private Set<String> diskCacheFileNameSet = Collections.newSetFromMap(new ConcurrentHashMap<String, Boolean>());
-    //private Map<String, Boolean> diskCacheMap = new ConcurrentHashMap<String, Boolean>(SIZE);
-
-    static {
-    }
-
-    // 用于存储
-    private List<CacheEntry> diskCacheList = Collections.synchronizedList(new ArrayList<CacheEntry>(SIZE));
 
     // synchronized 保证 map 和 list 同步；
     public synchronized void add(String url) {
         CacheEntry entry = new CacheEntry();
         entry.url = url;
-        diskCacheList.add(entry);
-        diskCacheUrlSet.add(url);
-        diskCacheFileNameSet.add(imgNameStrategy.getName(url));
+        addEntry(entry);
+
+        for (CacheEntry ce : diskCacheList) {
+            Log.d(StrongImageViewConstants.TAG, ce.getUrl() + " _ 4");
+        }
     }
 
     public synchronized boolean contains(String url) {
-        //return diskCacheMap.get(url) != null;
         return diskCacheUrlSet.contains(url);
     }
 
-    private String getFileName(String url) {
-        return dir + imgNameStrategy.getName(url);
+    public synchronized void removeEntry(CacheEntry entry) {
+        diskCacheUrlSet.remove(entry.url);
+        diskCacheFileNameSet.remove(imgNameStrategy.getName(entry.url));
+    }
+
+    public synchronized void addEntry(CacheEntry entry) {
+        if (!diskCacheUrlSet.contains(entry.url)) {
+            diskCacheList.add(entry);
+            diskCacheUrlSet.add(entry.url);
+            diskCacheFileNameSet.add(imgNameStrategy.getName(entry.url));
+        }
+    }
+
+    private File dumpFile() {
+        return new File(dir + "_" + dir.hashCode() + ".data");
     }
 
     class CacheEntry {
-        String url;
+        private String url;
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        @Override
+        public int hashCode() {
+            return url.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return o instanceof CacheEntry && url.hashCode() == ((CacheEntry) o).url.hashCode();
+        }
     }
 }
